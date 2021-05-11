16123ac659bcfc587fc4ba7f5d02e102512134f9
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-11 13:13:17 +0800

    almost there

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+    def index  
+        @tasks = Task.all
+    end
+
+    def new  
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        @task.save
+        redirect_to tasks_path
+    end
+
+    def show
+        if @task.completed
+            @message = "This task is completed"
+        else
+            @message = "This task is not completed yet"
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @task.update(task_params)
+        redirect_to @task
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_path
+    end
+
+    private
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
+
+    def task_params
+        params.require(:task).permit(:title, :details)
+    end
+end
